4284b4101f32693d47ca13345a2f09675b70645d
Yusuf <yusufsonmez951@gmail.com>
2023-07-01 14:19:21 +0300

    SinglyLinkedList tamamlandı

--- Program.cs
+++ Program.cs
@@ -14,30 +14,7 @@ namespace data_structures
     {
         static void Main(string[] args)
         {
-            var list = new List<int>() { 1,2,3,4,5};
-            var linkedList = new SinglyLinkedList<int>(list);
-
-            linkedList.Remove(1);
-            foreach (var item in linkedList)
-            {
-                Console.Write($"{item,-4}");
-            }
-            Console.WriteLine();
-            linkedList.Remove(5);
-            foreach (var item in linkedList)
-            {
-                Console.Write($"{item,-3}");
-            }
-
-            linkedList.Remove(3);
-            Console.WriteLine();
-            foreach (var item in linkedList)
-            {
-                Console.WriteLine(item);
-            }
-            linkedList.Remove(2);
-            linkedList.Remove(4);
-            Console.WriteLine(linkedList.Count());
+            
         }
     }
 }
